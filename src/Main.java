75cb8172df0f2715b32123b48e2b81c914ac7aa0
Khafiz <khafizturusbekuulu0@gmail.com>
2022-11-28 17:25:35 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Parallelepiped paralellepiped = new Parallelepiped();
+
+
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            try {
+                System.out.print("Бийиктиги:");
+                paralellepiped.setHeight(scanner.nextInt());
+                System.out.print("Узундугу:");
+                paralellepiped.setLength(scanner.nextInt());
+                System.out.print("Туурасы:");
+                paralellepiped.setWidth(scanner.nextInt());
+
+                if (paralellepiped.getHeight() < 0 || paralellepiped.getWidth() < 0 || paralellepiped.getLength() < 0) {
+                    System.out.println(("Терс сан койгонго болбойт!!!"));
+                } else if (paralellepiped.getHeight() > 20 || paralellepiped.getWidth() > 20 || paralellepiped.getLength() > 20) {
+                    throw new RuntimeException();
+                } else {
+                    System.out.println("Параллелепипеддин Аянты: " + paralellepiped.getSquare());
+                    System.out.println("Параллелепипеддин Колому: " + paralellepiped.getWeight());
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Тамга койгонго болбойт!!!");
+            }catch (RuntimeException e ){
+                System.out.println("Берилген сан 20 дан кичине болуусу керек!!!");
+            }
+        }
     }
 }
--- src/Parallelepiped.java
+++ src/Parallelepiped.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Parallelepiped {
+public class Parallelepiped  {
+    private int height;
+    private int length;
+    private int width;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+
+    public double getSquare(){
+        return  2*((height * length) + (length * width) + (height * width));
+    }
+    public double getWeight(){
+        return (length*width*height);
+    }
 }
